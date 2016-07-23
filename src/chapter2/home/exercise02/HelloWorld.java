bf1a508d74a8185a5fc174d905f3b44bba85eac1
KrusH <krushik@ukr.net>
2016-07-23 18:25:13 +0300

    hello

---
+++ src/chapter2/home/exercise02/HelloWorld.java
@@ -0,0 +1,10 @@
+// chapter2.book.HelloDate.java
+
+package chapter2.home.exercise02;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Привет Мир!");
+
+    }
+}
