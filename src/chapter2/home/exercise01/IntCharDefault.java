501ca28f3cef9fb2768a8f0dd31bfcc7753aea9d
KrusH <krushik@ukr.net>
2016-07-23 18:07:56 +0300

    Initial commit

---
+++ src/chapter2/home/exercise01/IntCharDefault.java
@@ -0,0 +1,19 @@
+package chapter2.home.exercise01;
+/*
+Создайте класс с полями int и char, которые не инициализируются в программе.
+Выведите их значение, что бы убедится в том, что Java выполняет инициализацию по умолчанию.
+*/
+public class IntCharDefault {
+
+    int i;
+    char c;
+
+    public IntCharDefault() {
+        System.out.println("i: " + i);
+        System.out.println("c: " + c);
+    }
+
+    public static void main(String[] args) {
+        new IntCharDefault();
+    }
+}
