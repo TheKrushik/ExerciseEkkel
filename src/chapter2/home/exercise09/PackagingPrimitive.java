0c7e2c9930129b2b81b71bf905b81a8a8716d44c
KrusH <krushik@ukr.net>
2016-07-24 14:36:45 +0300

    09-12

---
+++ src/chapter2/home/exercise09/PackagingPrimitive.java
@@ -0,0 +1,13 @@
+package chapter2.home.exercise09;
+
+import java.util.ArrayList;
+
+public class PackagingPrimitive {
+    public static void main(String[] args) {
+
+        Character ch = 'x';
+        char c = ch;
+        System.out.println(c);
+
+    }
+}
---
+++ src/chapter2/home/exercise10/ParameterCommandLine.java
@@ -0,0 +1,14 @@
+package chapter2.home.exercise10;
+
+public class ParameterCommandLine {
+
+    public static void main(String[] args) {
+
+        String arr [] = {"aaa", "bbb", "ccc"};
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+
+    }
+}
